a1ef868cf83298eafa7550163935b23e72e7cb37
Standa Pejša <standap@gmail.com>
2024-09-24 14:06:21 -0400

    making the package

---
+++ data-raw/newEurope_articles_singleGPE.R
@@ -0,0 +1,18 @@
+## code to prepare `newEurope_articles`
+
+library(tidyverse)
+
+tne_articles <- read_csv("data-raw/newEurope_articles_singleGPE.csv") |> 
+  arrange(doc_id)
+
+usethis::use_data(tne_articles, overwrite = TRUE)
+
+
+tne_entities <- read_csv("data-raw/cnlp_entities.csv") |> 
+  arrange(doc_id)
+usethis::use_data(tne_entities, overwrite = TRUE)
+
+tne_countries <- read_csv("data-raw/newEurope_quanteda_all_countries.csv") |> 
+  arrange(doc_id)
+
+read_csv("data-raw/top_15contributors_TNE.csv")
